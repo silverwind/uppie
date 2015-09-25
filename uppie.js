3a47e272edb056a4845b0fd02a1e1babe8cccf0e
silverwind <me@silverwind.io>
2015-09-25 17:26:15 +0200

    update for new linter rules

--- uppie.js
+++ uppie.js
@@ -1,19 +1,18 @@
 /*! uppie | (c) 2015 silverwind | BSD license */
 /* eslint-env browser, commonjs, amd */
-/* eslint-disable strict */
-(function (m) {
+(function(m) {
   if (typeof exports === "object" && typeof module === "object")
     module.exports = m();
   else if (typeof define === "function" && define.amd)
     return define([], m);
   else
     this.Uppie = m();
-})(function () {
+})(function() {
   "use strict";
   return function Uppie() {
-    return function (node, cb) {
+    return function(node, cb) {
       if (node.tagName.toLowerCase() === "input" && node.type === "file") {
-        node.addEventListener("change", function (event) {
+        node.addEventListener("change", function(event) {
           if ("getFilesAndDirectories" in event.target) {
             newDirectoryApi(event.target, cb.bind(null, event));
           } else {
@@ -26,10 +25,10 @@
           }
         });
       } else {
-        var stop = function (event) { event.preventDefault(); };
+        var stop = function(event) { event.preventDefault(); };
         node.addEventListener("dragover", stop);
         node.addEventListener("dragenter", stop);
-        node.addEventListener("drop", function (event) {
+        node.addEventListener("drop", function(event) {
           event.preventDefault();
           var dataTransfer = event.dataTransfer;
           if ("getFilesAndDirectories" in dataTransfer) {
@@ -45,12 +44,12 @@
   // API implemented in Firefox 42+ and Edge
   function newDirectoryApi(input, cb) {
     var fd = new FormData(), files = [];
-    var iterate = function (entries, path, resolve) {
+    var iterate = function(entries, path, resolve) {
       var promises = [];
-      entries.forEach(function (entry) {
-        promises.push(new Promise(function (resolve) {
+      entries.forEach(function(entry) {
+        promises.push(new Promise(function(resolve) {
           if ("getFilesAndDirectories" in entry) { // it's a directory
-            entry.getFilesAndDirectories().then(function (entries) {
+            entry.getFilesAndDirectories().then(function(entries) {
               iterate(entries, entry.path + entry.name + "/", resolve);
             });
           } else { // it's a file
@@ -65,8 +64,8 @@
       });
       Promise.all(promises).then(resolve.bind());
     };
-    input.getFilesAndDirectories().then(function (entries) {
-      new Promise(function (resolve) {
+    input.getFilesAndDirectories().then(function(entries) {
+      new Promise(function(resolve) {
         iterate(entries, "/", resolve);
       }).then(cb.bind(null, fd, files));
     });
@@ -75,7 +74,7 @@
   // old prefixed API implemented in Chrome 11+
   function oldDirectoryApi(input, cb) {
     var fd = new FormData(), files = [];
-    [].slice.call(input.files).forEach(function (file) {
+    [].slice.call(input.files).forEach(function(file) {
       fd.append("file", file, file.webkitRelativePath || file.name);
       files.push(file.webkitRelativePath || file.name);
     });
@@ -85,7 +84,7 @@
   // fallback for files without directories
   function multipleApi(input, cb) {
     var fd = new FormData(), files = [];
-    [].slice.call(input.files).forEach(function (file) {
+    [].slice.call(input.files).forEach(function(file) {
       fd.append("file", file, file.name);
       files.push(file.name);
     });
@@ -98,7 +97,7 @@
 
     function readEntries(entry, reader, oldEntries, cb) {
       var dirReader = reader || entry.createReader();
-      dirReader.readEntries(function (entries) {
+      dirReader.readEntries(function(entries) {
         var newEntries = oldEntries ? oldEntries.concat(entries) : entries;
         if (entries.length) {
           setTimeout(readEntries.bind(null, entry, dirReader, newEntries, cb), 0);
@@ -110,12 +109,12 @@
 
     function readDirectory(entry, path, resolve) {
       if (!path) path = entry.name;
-      readEntries(entry, undefined, undefined, function (entries) {
+      readEntries(entry, undefined, undefined, function(entries) {
         var promises = [];
-        entries.forEach(function (entry) {
-          promises.push(new Promise(function (resolve) {
+        entries.forEach(function(entry) {
+          promises.push(new Promise(function(resolve) {
             if (entry.isFile) {
-              entry.file(function (file) {
+              entry.file(function(file) {
                 fd.append("file", file, path + "/" + file.name);
                 files.push(path + "/" + file.name);
                 resolve();
@@ -127,12 +126,12 @@
       });
     }
 
-    [].slice.call(items).forEach(function (entry) {
+    [].slice.call(items).forEach(function(entry) {
       entry = entry.webkitGetAsEntry();
       if (entry) {
-        rootPromises.push(new Promise(function (resolve) {
+        rootPromises.push(new Promise(function(resolve) {
           if (entry.isFile) {
-            entry.file(function (file) {
+            entry.file(function(file) {
               fd.append("file", file, file.name);
               files.push(file.name);
               resolve();
