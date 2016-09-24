ec7d43f37c94984eee782d4d56b99cb4a4e45969
silverwind <me@silverwind.io>
2016-09-24 18:50:06 +0200

    bump version

--- uppie.js
+++ uppie.js
@@ -1,4 +1,4 @@
-/*! uppie v0.11.5 | (c) silverwind | BSD license */
+/*! uppie v0.11.6 | (c) silverwind | BSD license */
 /* eslint-env commonjs, amd */
 (function(m) {
   if (typeof exports === "object" && typeof module === "object")
