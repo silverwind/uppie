2ef9b80f47b59d6ad9d5ce83a8e300ba02f617fb
silverwind <me@silverwind.io>
2016-11-13 12:00:48 +0100

    bump version

--- uppie.js
+++ uppie.js
@@ -1,4 +1,4 @@
-/*! uppie v0.11.8 | (c) silverwind | BSD license */
+/*! uppie v0.11.9 | (c) silverwind | BSD license */
 (function(m) {
   if (typeof exports === "object" && typeof module === "object")
     module.exports = m();
