434f78023f9ce20c2627bb6fd21e181ee622a84e
silverwind <me@silverwind.io>
2018-06-03 18:14:38 +0200

    bump version

--- uppie.js
+++ uppie.js
@@ -1,4 +1,4 @@
-/*! uppie v1.0.9 | (c) silverwind | BSD license */
+/*! uppie v1.0.10 | (c) silverwind | BSD license */
 (function(root, m) {
   if (typeof define === "function" && define.amd) {
     define([], m);
